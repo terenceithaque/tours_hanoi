e739ec328320628c8725596efb9d3ebcbcc53df9
Terence Le Thierry <terence.lethierry@protonmail.com>
2026-01-25 15:42:15 +0100

    Affichage de la fenêtre de jeu

---
+++ jeu.py
@@ -0,0 +1,99 @@
+# Programme du jeu
+from pile import *
+import pygame
+pygame.init()
+
+
+
+class Jeu:
+    """Une partie du jeu des tours de Hanoï"""
+    def __init__(self, nb_colonnes:int=3, nb_disques:int=5) -> None:
+        """Crée une nouvelle instance du jeu des tours de Hanoï.
+        - nb_colonnes: nombre de colonnes,
+        - nb_disques: nombre de disques à déplacer."""
+
+        # Nombre de colonnes et de disques
+        self.nb_colonnes = nb_colonnes
+        self.nb_disques = nb_disques
+
+
+    def executer(self) -> None:
+        """Lance la boucle de jeu"""
+
+
+        self.fenetre = pygame.display.set_mode((800, 600))
+        pygame.display.set_caption("Tours de Hanoï")
+
+        # Créer les colonnes et insérer les disques dedans
+        colonnes = []
+
+        for i in range(self.nb_colonnes):
+            col = Pile(self.nb_disques)
+            colonnes.append(col)
+
+
+
+        for n in range(1, self.nb_disques + 1):
+            colonnes[0].empiler(n)
+
+
+        execution = True    
+
+        # Boucle prinicipale
+        while execution:
+
+            for evenement in pygame.event.get():
+                if evenement.type == pygame.QUIT:
+                    execution = False
+
+
+            """
+            # Colonnes depuis lesquelles le joueur peut effectuer un mouvement
+            colonnes_mouvement_pos = [col for col in colonnes if not col.est_vide()]
+
+            # Colonnes pleines
+            colonnes_pleines = [col for col in colonnes if col.est_pleine()]
+
+            col_depart = int(input("Depuis quelle colonne faire un mouvement ? "))
+            while not colonnes[col_depart - 1] in colonnes_mouvement_pos:
+                print("Impossible ! Colonne vide.")
+                col_depart = int(input("Depuis quelle colonne faire un mouvement ? "))
+
+
+            col_arrivee = int(input("Vers quelle colonne doit se faire le mouvement ? ")) 
+            while colonnes[col_arrivee - 1] in colonnes_pleines:
+                print("Impossible ! Colonne de destination pleine.")
+                col_arrivee = int(input("Vers quelle colonne doit se faire le mouvement ? "))
+
+
+            colonne_depart = colonnes[col_depart - 1]
+            colonne_arrivee = colonnes[col_arrivee - 1]
+            print(colonne_depart.contenu, colonne_arrivee.contenu)    
+
+
+            # Effectuer le mouvement
+            disque_depart = colonne_depart.depiler() # Disque présent sur la colonne de départ
+            if not colonne_arrivee.est_vide():
+                disque_arrivee = colonne_arrivee.depiler() # On dépile le disque présent au sommet de la colonne d'arrivée
+                # Détecter les déplacements illégaux
+                if disque_arrivee < disque_depart:
+                    print("Déplacement interdit !")
+                    colonne_arrivee.empiler(disque_arrivee) # Ré-empiler le disque présent dans la colonne d'arrivée
+                    colonne_depart.empiler(disque_depart)
+
+                else:
+                    # Ré-empiler le disque présent dans la colonne d'arrivée, puis empiler le disque de la colonne de départ par-dessus
+                    colonne_arrivee.empiler(disque_arrivee)
+                    colonne_arrivee.empiler(disque_depart)
+
+            else:
+                colonne_arrivee.empiler(disque_depart)"""
+
+
+            pygame.display.flip()             
+
+
+
+
+
+              
--- main.py
+++ main.py
@@ -1,83 +1,16 @@
-# Programme principal
-from pile import *
+# Script de lancement du jeu
+import pygame
+from tkinter import simpledialog
+from jeu import *
+pygame.init()
 
 
-# Demander le nombre de colonnes (piles)
-nb_colonnes = int(input("Combien de colonnes souhaitez-vous ? "))
-nb_disques = int(input("Combien de disques souhaitez-vous ? "))
-
-colonnes = []
-
-for i in range(nb_colonnes):
-    col = Pile(nb_disques)
-    colonnes.append(col)
+# Demander au joueur le nombre de colonnes et de disques
+nb_colonnes = simpledialog.askinteger("Nombre de colonnes", "Saisissez le nombre de colonnes: ")
+nb_disques = simpledialog.askinteger("Nombre de disques", "Saisissez le nombre de disques: ")
 
 
-
-for n in range(1, nb_disques + 1):
-    colonnes[0].empiler(n)
-
-def afficher_colonnes():
-    """Affiche les colonnes du jeu"""
-    for i in range(len(colonnes)):
-        print(f"Colonne n° {i+1}")
-        print(colonnes[i])
-        #print(end=" ")
+jeu = Jeu(nb_colonnes, nb_disques)
+jeu.executer()
 
 
-
-def jeu():
-    """Boucle principale du jeu."""
-
-    # Le jeu se poursuit tant que la dernière colonne n'est pas remplie
-    while not colonnes[-1].est_pleine():
-
-        for colonne in colonnes:
-            afficher_colonnes()
-
-        # Colonnes depuis lesquelles le joueur peut effectuer un mouvement
-        colonnes_mouvement_pos = [col for col in colonnes if not col.est_vide()]
-
-        # Colonnes pleines
-        colonnes_pleines = [col for col in colonnes if col.est_pleine()]
-
-        col_depart = int(input("Depuis quelle colonne faire un mouvement ? "))
-        while not colonnes[col_depart - 1] in colonnes_mouvement_pos:
-            print("Impossible ! Colonne vide.")
-            col_depart = int(input("Depuis quelle colonne faire un mouvement ? "))
-
-
-        col_arrivee = int(input("Vers quelle colonne doit se faire le mouvement ? ")) 
-        while colonnes[col_arrivee - 1] in colonnes_pleines:
-            print("Impossible ! Colonne de destination pleine.")
-            col_arrivee = int(input("Vers quelle colonne doit se faire le mouvement ? "))
-
-
-        colonne_depart = colonnes[col_depart - 1]
-        colonne_arrivee = colonnes[col_arrivee - 1]
-        print(colonne_depart.contenu, colonne_arrivee.contenu)    
-
-
-        # Effectuer le mouvement
-        disque_depart = colonne_depart.depiler() # Disque présent sur la colonne de départ
-        if not colonne_arrivee.est_vide():
-            disque_arrivee = colonne_arrivee.depiler() # On dépile le disque présent au sommet de la colonne d'arrivée
-            # Détecter les déplacements illégaux
-            if disque_arrivee < disque_depart:
-                print("Déplacement interdit !")
-                colonne_arrivee.empiler(disque_arrivee) # Ré-empiler le disque présent dans la colonne d'arrivée
-                colonne_depart.empiler(disque_depart)
-
-            else:
-                # Ré-empiler le disque présent dans la colonne d'arrivée, puis empiler le disque de la colonne de départ par-dessus
-                colonne_arrivee.empiler(disque_arrivee)
-                colonne_arrivee.empiler(disque_depart)
-
-        else:
-            colonne_arrivee.empiler(disque_depart)   
-
-
-
-jeu()
-
-              
